6470877f6470f6b0fc377f7c301fb6e9d794d98d
tyler knapp <tgknapp11@gmail.com>
2019-06-04 01:48:34 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,36 @@
+function forLoop(array) {
+  for (i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array;
+}
+
+//////
+
+function whileLoop(n) {
+  let countdown = n;
+
+  while (countdown > 0) {
+    console.log(--countdown)
+  }
+  return "done";
+}
+
+//////
+
+var num = 5;
+
+function doWhileLoop(num){
+
+     function incrementVariable() {
+     i = i++;
+    }
+
+    do {
+     console.log("I run once regardless.");
+   } while (incrementVariable() < num);
+  }
